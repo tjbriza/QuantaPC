91d9c48f925eebfca56e8d943d149b4b22056735
nmarcusg <89849200+nmarcusg@users.noreply.github.com>
2025-07-19 23:14:23 +0800

    added login-logout with proper routing

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,16 +1,43 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { UserAuth } from '../../context/AuthContext.jsx';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const { signInUser } = UserAuth();
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const result = await signInUser(email, password);
+
+      if (result.success) {
+        navigate('/dashboard');
+      } else {
+        setError(result.error.message);
+      }
+    } catch (error) {
+      setError(
+        'An unexpected error occurred. Please try again: ' + error.message
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <>
       <main>
         <form
           className='flex flex-col items-center justify-center gap-5'
-          action=''
+          onSubmit={handleLogin}
         >
           <h2 className='text-4xl font-semibold'>
             Log in to your QuantaPC account
@@ -23,6 +50,8 @@ export default function Login() {
               className='border-2 p-2 rounded-sm'
               type='text'
               placeholder='Email'
+              onChange={(e) => setEmail(e.target.value)}
+              required={true}
             />
           </div>
           <div className='flex flex-col items-center justify-center gap-2'>
@@ -33,16 +62,15 @@ export default function Login() {
               className='border-2 p-2 rounded-sm'
               type='password'
               placeholder='Password'
+              onChange={(e) => setPassword(e.target.value)}
+              required={true}
             />
           </div>
 
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-            }}
-          >
+          <button type='submit' disabled={loading}>
             Submit
           </button>
+          {error && <p>error has occured: {error}</p>}
         </form>
       </main>
     </>
--- src/components/ui/Navigation.jsx
+++ src/components/ui/Navigation.jsx
@@ -1,12 +1,27 @@
 import { Link } from 'react-router-dom';
+import { UserAuth } from '../../context/AuthContext';
 
 export default function Navigation() {
+  const { session, signOut } = UserAuth();
+
   return (
     <nav className='flex gap-4 p-4'>
       <Link to='/'>Home</Link>
-      <Link to='/login'>Login</Link>
-      <Link to='/signup'>Sign Up</Link>
-      <Link to='/dashboard'>Dashboard</Link>
+      {session ? (
+        <>
+          <Link to='/dashboard'>Dashboard</Link>
+          <button onClick={signOut} className='cursor-pointer'>
+            Logout
+          </button>
+        </>
+      ) : (
+        <>
+          <Link to='/login'>Login</Link>
+          <Link to='/signup'>Sign Up</Link>
+        </>
+      )}
+
+      <p>Status: {session ? 'Logged in' : 'Logged out'}</p>
     </nav>
   );
 }
