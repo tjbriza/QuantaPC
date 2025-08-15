027aa6c48fc176f5baf5a31707000c7f357210cc
nmarcusg <89849200+nmarcusg@users.noreply.github.com>
2025-08-15 18:14:02 +0800

    edited wishlist page content

--- src/components/pages/Dashboard/Wishlist.jsx
+++ src/components/pages/Dashboard/Wishlist.jsx
@@ -1,7 +1,7 @@
 export default function Wishlist() {
   return (
     <div className='min-h-screen flex items-center justify-center mt-16'>
-      <h1 className='text-2xl font-bold'>Wishlist Page</h1>
+      <h1 className='text-2xl font-bold'>Wishlist</h1>
     </div>
   );
 }
