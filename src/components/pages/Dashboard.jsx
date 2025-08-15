365974938a1c6bdf15ffd3f16cb2ffdb6c1a3156
nmarcusg <89849200+nmarcusg@users.noreply.github.com>
2025-08-15 17:53:53 +0800

    added dashboard navigation, outlet for dashboard subdirectories

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,19 +1,18 @@
 import { useState } from 'react';
-import Navigation from '../ui/Navigation';
 import { useAuth } from '../../context/AuthContext';
 import { useSupabaseRead } from '../../hooks/useSupabaseRead';
-import { Navigate } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import DashboardNavigation from '../ui/Dashboard/DashboardNavigation';
 
 export default function Dashboard() {
   const { session } = useAuth();
-
   const { data, error, loading } = useSupabaseRead('profiles', {
     filter: { id: session?.user.id },
     single: true,
   });
 
   const userProfile = data || {};
-  //check if user doesn't have a  (temporary check)
+  //check if user doesn't have a  (temporary check, should be replaced with a more robust check!11)
   if (!loading) {
     if (userProfile.name_first === undefined) {
       return <Navigate to='/profilesetup' replace />;
@@ -21,16 +20,11 @@ export default function Dashboard() {
   }
 
   return (
-    <>
-      <h1>Dashboard</h1>
-      <p>Welcome, {session ? userProfile.name_first : 'not logged in'}!</p>
-      <div>
-        {userProfile.avatar_url ? (
-          <img src={userProfile.avatar_url} />
-        ) : (
-          'no pic bro'
-        )}
+    <div className='min-h-screen flex item-center mt-16'>
+      <div className='container mx-auto px-4'>
+        <DashboardNavigation />
+        <Outlet />
       </div>
-    </>
+    </div>
   );
 }
