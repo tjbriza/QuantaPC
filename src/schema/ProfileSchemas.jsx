0f2df437f965eee5050f57d8ba663d87b2fa604b
nmarcusg <89849200+nmarcusg@users.noreply.github.com>
2025-08-22 17:06:44 +0800

    added max constraints and special character limitations on zod forms

--- src/schema/ProfileSchemas.jsx
+++ src/schema/ProfileSchemas.jsx
@@ -1,16 +1,28 @@
 import { z } from 'zod';
 
 export const profileSchema = z.object({
-  username: z.string().min(1, 'Username is required'),
-  name_first: z.string().min(1, 'First name is required'),
-  name_last: z.string().min(1, 'Last name is required'),
+  username: z
+    .string()
+    .min(3, 'At least 3 characters for Username is required')
+    .max(20, 'Username too long'),
+  name_first: z
+    .string()
+    .min(2, 'At least 2 characters for first name is required')
+    .max(30, 'First name too long')
+    .regex(/^[A-Za-z]+$/, 'Only letters allowed'),
+  name_last: z
+    .string()
+    .min(2, 'At least 2 characters for last name is required')
+    .max(30, 'Last name too long')
+    .regex(/^[A-Za-z]+$/, 'Only letters allowed'),
 });
 
 export const addressSchema = z.object({
   full_name: z
     .string()
     .min(1, 'Full name is required')
-    .max(100, 'Full Name is too long'),
+    .max(70, 'Full name too long')
+    .regex(/^[A-Za-z\s.]+$/, 'Only letters, spaces, and periods allowed'),
   phone_number: z
     .string()
     .min(1, 'Phone number is required')
@@ -21,16 +33,22 @@ export const addressSchema = z.object({
   country: z
     .string()
     .min(1, 'Country is required')
-    .max(50, 'Country name too long'),
+    .max(30, 'Country name too long')
+    .regex(/^[A-Za-z\s]+$/, 'Only letters allowed'),
   region: z
     .string()
     .min(1, 'Region is required')
-    .max(50, 'Region name too long'),
+    .max(60, 'Region name too long'),
   province: z
     .string()
     .min(1, 'Province is required')
-    .max(50, 'Province name too long'),
-  city: z.string().min(1, 'City is required').max(50, 'City name too long'),
+    .max(30, 'Province name too long')
+    .regex(/^[A-Za-z\s]+$/, 'Only letters allowed'),
+  city: z
+    .string()
+    .min(1, 'City is required')
+    .max(30, 'City name too long')
+    .regex(/^[A-Za-z\s]+$/, 'Only letters allowed'),
   barangay: z
     .string()
     .min(1, 'Barangay is required')
@@ -42,12 +60,12 @@ export const addressSchema = z.object({
   street_name: z
     .string()
     .min(1, 'Street name is required')
-    .max(100, 'Street name too long'),
-  building_name: z.string().max(100, 'Building name too long').optional(),
+    .max(80, 'Street name too long'),
+  building_name: z.string().max(80, 'Building name too long').optional(),
   house_number: z
     .string()
     .min(1, 'House number is required')
-    .max(20, 'House number too long'),
+    .max(15, 'House number too long'),
   address_label: z
     .string()
     .min(1, 'Address label is required')
