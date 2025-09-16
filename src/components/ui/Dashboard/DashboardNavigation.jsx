7ab30734c4b2da71ea0a1890ba18908097cee7e6
nmarcusg <89849200+nmarcusg@users.noreply.github.com>
2025-09-16 12:49:21 +0800

    added navigation for user services tab

--- src/components/ui/Dashboard/DashboardNavigation.jsx
+++ src/components/ui/Dashboard/DashboardNavigation.jsx
@@ -3,18 +3,19 @@ import { NavLink } from 'react-router-dom';
 const navItems = [
   { to: 'profile', label: 'Account Details' },
   { to: 'orders', label: 'My Orders' },
+  { to: 'services', label: 'My Services' },
   { to: 'addresses', label: 'My Addresses' },
   { to: 'wishlist', label: 'Wishlist' },
 ];
 
 export default function DashboardNavigation() {
   return (
-    <div className="flex flex-col lg:flex-row items-start mt-20 mb-8 gap-8 lg:gap-[104px]">
-      <h1 className="hero-main-title font-bold leading-tight antialiased font-heading text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-[#282E41]">
+    <div className='flex flex-col lg:flex-row items-start mt-20 mb-8 gap-8 lg:gap-[104px]'>
+      <h1 className='hero-main-title font-bold leading-tight antialiased font-heading text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-[#282E41]'>
         DASHBOARD
       </h1>
-      <nav className="mt-8">
-        <ul className="flex flex-col lg:flex-row items-center gap-8 lg:gap-20 text-2xl">
+      <nav className='mt-8'>
+        <ul className='flex flex-col lg:flex-row items-center gap-8 lg:gap-20 text-2xl'>
           {navItems.map(({ to, label }) => (
             <li key={to}>
               <NavLink
